a0d8a113f5477a5905306322744b58f2b49fc957
Rahul Nair <rnair1199@gmail.com>
2023-04-02 01:16:00 +0100

    testing fix for submitBrandSearch function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,10 +16,12 @@ const Home: NextPage = () => {
     setSearchbrand(event.target.value);
   }
 
-  const submitBrandSearch = (event: FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
+  function brandSearch() {
+    router.push(`/${searchBrand}`);
+  }
 
-    void router.push(`/${searchBrand}`);
+  function preventDefaultSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
   }
 
   return (
@@ -31,19 +33,20 @@ const Home: NextPage = () => {
       </Head>
       <main className="hidden lg:flex flex-col justify-center items-center text-center ">
         <form 
-          className='flex flex-col pb-[5rem] pt-[3rem] items-center justify-center text-center font-light'
-          onSubmit={submitBrandSearch}
+          className='flex flex-col pb-[5rem] pt-[2rem] items-center justify-center text-center font-light'
+          onSubmit={preventDefaultSubmit}
         >
           <input 
             type='text' 
             className='text-2xl w-[15rem] outline-none bg-1c1b1c mb-4'
             placeholder='*.redcatcloud.com.au' 
             spellCheck='false' 
-            onChange={void handleBrandSearch}
+            onChange={handleBrandSearch}
           />
 
           <button
             className="bg-20222e px-2 py-1 rounded-2xl text-sm border border-282a36 hover:bg-2f334a hover:border-5e4fb3/40 transition duration-500"
+            onClick={brandSearch}
           >
             Search
           </button>
