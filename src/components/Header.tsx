dc4021094d783bf0025acdc0f3aa7218df6b5b28
Rahul Nair <rnair1199@gmail.com>
2023-04-17 11:43:10 +0100

    added link tag for feed icon on header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -155,15 +155,17 @@ const Header = ({ togglePostWizard, handlePostWizardToggle }: HeaderProps) => {
                     ) : (
                         <>
                             {userEmail.includes("@redcat.com.au") || userEmail === "rnair1199@gmail.com" && (
-                                <svg 
-                                    xmlns="http://www.w3.org/2000/svg" 
-                                    viewBox="0 0 20 20" 
-                                    fill="currentColor" 
-                                    className="w-5 h-5 m-[0.5rem] text-ffffff/70 hover:text-ffffff transform transition duration-500 cursor-pointer hover:scale-110"
-                                    onClick={(() => router.push("/feed"))}
-                                >
-                                    <path fillRule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clipRule="evenodd" />
-                                </svg>
+                                <Link href='/feed'>
+                                    <svg 
+                                        xmlns="http://www.w3.org/2000/svg" 
+                                        viewBox="0 0 20 20" 
+                                        fill="currentColor" 
+                                        className="w-5 h-5 m-[0.5rem] text-ffffff/70 hover:text-ffffff transform transition duration-500 cursor-pointer hover:scale-110"
+                                    >
+                                        <path fillRule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clipRule="evenodd" />
+                                    </svg>
+                                </Link>
+
                             )}
                         </>
                     )}
